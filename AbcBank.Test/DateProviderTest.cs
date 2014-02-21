6cf962ae6585e7c4873d4bb8c1cc2ec1dc45d26f
scheng@levadany.com <scheng@levadany.com>
2014-02-21 16:55:26 -0500

    Added a dummy date provider

---
+++ AbcBank.Test/DateProviderTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbcBank.Test
+{
+    [TestFixture]
+    public class DateProviderTest
+    {
+        [Test]
+        public void TestDummyDateProvider()
+        {
+            DummyDateProvider.SetNow("1/1/2001");
+            Assert.AreEqual("1/1/2001", DummyDateProvider.GetInstance().Now().ToShortDateString());
+        }
+    }
+}
---
+++ AbcBank.Test/DummyDateProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbcBank.Interfaces;
+using AbcBank.Enums;
+
+namespace AbcBank.Test
+{
+    public class DummyDateProvider : iDateProvider
+    {
+        private static DummyDateProvider instance = new DummyDateProvider();
+        private static DateTime _date;
+
+        public static DummyDateProvider GetInstance()
+        {
+            return instance;
+        }
+
+        public static void SetNow(string date) {
+            _date = DateTime.Parse(date);
+        }
+
+        public DateTime Now()
+        {
+            return _date;
+        }
+    }
+}
