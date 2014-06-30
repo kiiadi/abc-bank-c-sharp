12e4943b14695cf5b1255c339d196e8e836295ff
gvolpert <gvolpert@gmail.com>
2014-06-29 21:39:44 -0400

    Update Account.cs

--- AbcBank/Account.cs
+++ AbcBank/Account.cs
@@ -1,92 +1,123 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AbcBank
 {
     public class Account
     {
-
-        public const int CHECKING = 0;
-        public const int SAVINGS = 1;
-        public const int MAXI_SAVINGS = 2;
-
-        private readonly int accountType;
-        public List<Transaction> transactions;
-
-        public Account(int accountType)
+        #region nested types
+        public enum AccountType
         {
-            this.accountType = accountType;
-            this.transactions = new List<Transaction>();
+            Checking,
+            Saving,
+            MaxiSaving
         }
+        #endregion
 
-        public void deposit(double amount)
+        #region private fields
+        private readonly AccountType _accountType;
+        private List<Transaction> _transactions = new List<Transaction>();
+        #endregion
+
+        #region ctor(s)
+        public Account(AccountType accountType)
         {
-            if (amount <= 0)
-            {
-                throw new ArgumentException("amount must be greater than zero");
-            }
-            else
-            {
-                transactions.Add(new Transaction(amount));
-            }
+            _accountType = accountType;
         }
+        #endregion
 
-        public void withdraw(double amount)
+        #region public methods/properties
+        public IEnumerable<Transaction> Transactions { get { return _transactions; } }
+        public AccountType Type { get { return _accountType; } }
+        public void Deposit(double amount)
         {
-            if (amount <= 0)
-            {
-                throw new ArgumentException("amount must be greater than zero");
-            }
-            else
-            {
-                transactions.Add(new Transaction(-amount));
-            }
+            DepositImpl(amount);
         }
-
-        public double interestEarned()
+        public void Withdraw(double amount)
         {
-            double amount = sumTransactions();
-            switch (accountType)
+            DepositImpl(-amount);
+        }
+        public double InterestEarned()
+        {
+            double amount = SumTransactions();
+            switch (_accountType)
             {
-                case SAVINGS:
-                    if (amount <= 1000)
-                        return amount * 0.001;
-                    else
-                        return 1 + (amount - 1000) * 0.002;
+                case AccountType.Saving:
+                    return GetSavingInterest(amount);
                 // case SUPER_SAVINGS:
                 //     if (amount <= 4000)
                 //         return 20;
-                case MAXI_SAVINGS:
-                    if (amount <= 1000)
-                        return amount * 0.02;
-                    if (amount <= 2000)
-                        return 20 + (amount - 1000) * 0.05;
-                    return 70 + (amount - 2000) * 0.1;
+                case AccountType.MaxiSaving:
+                    return GetMaxiSavingInterest(amount);
+                case AccountType.Checking:
+                    return GetCheckingInterest(amount);
                 default:
-                    return amount * 0.001;
+                    throw new InvalidOperationException(string.Format("Unknown account type: {0}", _accountType));
             }
         }
-
-        public double sumTransactions()
+        public double SumTransactions()
         {
-            return checkIfTransactionsExist(true);
+            return _transactions.Select(t => t.Amount).Sum();
         }
-
-        private double checkIfTransactionsExist(bool checkAll)
+        public String BuildStatement()
         {
-            double amount = 0.0;
-            foreach (Transaction t in transactions)
-                amount += t.amount;
-            return amount;
-        }
+            StringBuilder s = new StringBuilder();
 
-        public int getAccountType()
+            //Translate to pretty account type
+            switch (_accountType)
+            {
+                case Account.AccountType.Checking:
+                    s.AppendLine("Checking Account");
+                    break;
+                case Account.AccountType.Saving:
+                    s.AppendLine("Savings Account");
+                    break;
+                case Account.AccountType.MaxiSaving:
+                    s.AppendLine("Maxi Savings Account");
+                    break;
+            }
+
+            //Now total up all the transactions
+            double total = 0.0;
+            foreach (Transaction t in _transactions)
+            {
+                s.AppendFormat("  {0} {1}\n", (t.Amount < 0 ? "withdrawal" : "deposit"), t.Amount.ToDollars());
+                total += t.Amount;
+            }
+            s.AppendFormat("Total {0}", total.ToDollars());
+            return s.ToString();
+        }
+        #endregion
+
+        #region private methods
+        private void DepositImpl(double amount)
         {
-            return accountType;
-        }
+            if (amount <= 0)
+                throw new ArgumentException("amount must be greater than zero");
 
+            _transactions.Add(new Transaction(amount));
+        }
+        private static double GetSavingInterest(double amount)
+        {
+            if (amount <= 1000)
+                return amount * 0.001;
+
+            return 1 + (amount - 1000) * 0.002;
+        }
+        private static double GetMaxiSavingInterest(double amount)
+        {
+            if (amount <= 1000)
+                return amount * 0.02;
+            if (amount <= 2000)
+                return 20 + (amount - 1000) * 0.05;
+            return 70 + (amount - 2000) * 0.1;
+        }
+        private static double GetCheckingInterest(double amount)
+        {
+            return amount * 0.001;
+        }
+        #endregion
     }
 }
