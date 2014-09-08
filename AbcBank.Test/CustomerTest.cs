178f23ec2d538da75a2d1ed61b3944f47bba21ce
AlexeyYahoo <aazengin@yahoo.com>
2014-09-08 12:02:54 -0400

    abc-bank-c-sharp test completed.  Code refactored.  Test cases added.  Rates accrual changed.  Transfer between accoubts added.  For further info see README_AAZ_Changes.md file.

--- AbcBank.Test/CustomerTest.cs
+++ AbcBank.Test/CustomerTest.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace AbcBank.Test
@@ -10,57 +6,74 @@ namespace AbcBank.Test
     [TestFixture]
     public class CustomerTest
     {
-
-        [Test] //Test customer statement generation
-        public void testApp()
+        [Test]
+        public void CustomerStatementGeneration()
         {
+            Account checkingAccount = new Account(AccountType.Checking);
+            Account savingsAccount = new Account(AccountType.Savings);
+            Account maxiSavingsAccount = new Account(AccountType.Maxi_Savings);
 
-            Account checkingAccount = new Account(Account.CHECKING);
-            Account savingsAccount = new Account(Account.SAVINGS);
+            Customer henry = new Customer("Henry");
+            henry.OpenAccount(checkingAccount);
+            henry.OpenAccount(savingsAccount);
+            henry.OpenAccount(maxiSavingsAccount);
 
-            Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
+            checkingAccount.Deposit(100.0);
+            savingsAccount.Deposit(4000.0);
+            savingsAccount.Withdraw(200.0);
 
-            checkingAccount.deposit(100.0);
-            savingsAccount.deposit(4000.0);
-            savingsAccount.withdraw(200.0);
-
-            Assert.AreEqual("Statement for Henry\n" +
-                    "\n" +
-                    "Checking Account\n" +
-                    "  deposit $100.00\n" +
-                    "Total $100.00\n" +
-                    "\n" +
-                    "Savings Account\n" +
-                    "  deposit $4,000.00\n" +
-                    "  withdrawal $200.00\n" +
-                    "Total $3,800.00\n" +
-                    "\n" +
-                    "Total In All Accounts $3,900.00", henry.getStatement());
+            String customerStatement = "Statement for Henry\n" +
+                               "\n" +
+                               "Checking Account\n" +
+                               "  deposit $100.00\n" +
+                               "Total $100.00\n" +
+                               "\n" +
+                               "Savings Account\n" +
+                               "  deposit $4,000.00\n" +
+                               "  withdrawal $200.00\n" +
+                               "Total $3,800.00\n" +
+                               "\n" +
+                               "Maxi-Savings Account\n" +
+                               "Total $0.00\n" +
+                               "\n" +
+                               "Total In All Accounts $3,900.00";
+            Console.WriteLine(customerStatement);
+            Assert.AreEqual(customerStatement, henry.GetStatement());
         }
 
         [Test]
-        public void testOneAccount()
+        public void CustomerWithNoAccounts()
         {
-            Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
-            Assert.AreEqual(1, oscar.getNumberOfAccounts());
+            Customer oscar = new Customer("Oscar");
+            Assert.AreEqual(0, oscar.NumberOfAccounts);
         }
 
         [Test]
-        public void testTwoAccount()
+        public void CustomerHavingOneAccount()
         {
-            Customer oscar = new Customer("Oscar")
-                    .openAccount(new Account(Account.SAVINGS));
-            oscar.openAccount(new Account(Account.CHECKING));
-            Assert.AreEqual(2, oscar.getNumberOfAccounts());
+            Customer oscar = new Customer("Oscar");
+            Account account = new Account(AccountType.Savings);
+            oscar.OpenAccount(account);
+            Assert.AreEqual(1, oscar.NumberOfAccounts);
         }
 
-        [Ignore]
-        public void testThreeAcounts()
+        [Test]
+        public void CustomerHavingMoreThanOneAccount()
         {
-            Customer oscar = new Customer("Oscar")
-                    .openAccount(new Account(Account.SAVINGS));
-            oscar.openAccount(new Account(Account.CHECKING));
-            Assert.AreEqual(3, oscar.getNumberOfAccounts());
+            Customer oscar = new Customer("Oscar");
+            oscar.OpenAccount(new Account(AccountType.Savings));
+            oscar.OpenAccount(new Account(AccountType.Checking));
+            Assert.AreEqual(2, oscar.NumberOfAccounts);
+        }
+
+        [Test]
+        public void CustomerHavingMoreThanOneAccountOfTheSameType()
+        {
+            Customer oscar = new Customer("Oscar");
+            oscar.OpenAccount(new Account(AccountType.Maxi_Savings));
+            oscar.OpenAccount(new Account(AccountType.Checking));
+            oscar.OpenAccount(new Account(AccountType.Maxi_Savings));
+            Assert.AreEqual(3, oscar.NumberOfAccounts);
         }
     }
 }
